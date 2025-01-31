3cbbb6c383b078d844db2f271a7ab17f0140db7a
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-01-31 10:49:04 -0500

    created a post list

--- src/data/users.ts
+++ src/data/users.ts
@@ -17,8 +17,45 @@ const user = {
     username: '_ipsom55',
     name: 'Ipsom',
     profPic: 'DarkOrchid',
-    des: "Loror ipsom unet lau. Sporor. Jipum. Kelent."
+    des: "Loror ipsom unet lau. Sporor. Jipum. Kelent.",
+    followers: randomNum(),
+    following: randomNum(),
+
 }
+
+const posts:Post[] = [
+    {
+        img: "DarkKhaki",
+        likes: randomNum(),
+        comments: randomNum(),
+    },
+    {
+        img: "DarkOrange",
+        likes: randomNum(),
+        comments: randomNum(),
+    },
+    {
+        img: "FireBrick",
+        likes: randomNum(),
+        comments: randomNum(),
+    },
+    {
+        img: "DodgerBlue",
+        likes: randomNum(),
+        comments: randomNum(),
+    },
+
+    {
+        img: "Indigo",
+        likes: randomNum(),
+        comments: randomNum(),
+    },
+    {
+        img: "MintCream",
+        likes: randomNum(),
+        comments: randomNum(),
+    },
+]
 
 function randomNum() {
     return Math.floor(Math.random()*100)
