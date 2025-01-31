15227c8dec39485d804bae059c267e655d9c6452
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-01-31 11:06:27 -0500

    created props type, added props, added some inline styles

--- src/components/ProfilePic.tsx
+++ src/components/ProfilePic.tsx
@@ -1,10 +1,22 @@
 import { user } from "../data/users"
 
+type Props = {
+    height: string,
+    width: string,
+}
+
 const pic = user.profPic;
 
-const ProfilePic = () => {
+
+const ProfilePic = (props:Props) => {
+    const {height, width} = props
   return (
-    <div>ProfilePic</div>
+    <div style={{backgroundColor: `${pic}`, 
+        borderRadius: '50%',
+        height: `${height}`,
+        width: `${width}`}
+        }>
+    </div>
   )
 }
 
