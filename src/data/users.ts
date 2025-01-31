09f1768a0884c9a30507b2e6236ce762a5f91eba
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-01-31 10:02:49 -0500

    created initial user

--- src/data/users.ts
+++ src/data/users.ts
@@ -2,6 +2,7 @@ type User = {
     username: string,
     name: string,
     profPic: string,
+    des: string,
     followers: number,
     following: number,
     postList: Post[],
@@ -10,4 +11,11 @@ type Post = {
     img: string,
     likes: number,
     comments: number,
+}
+
+const user = {
+    username: '_ipsom55',
+    name: 'Ipsom',
+    profPic: 'DarkOrchid',
+    des: "Loror ipsom unet lau. Sporor. Jipum. Kelent."
 }
