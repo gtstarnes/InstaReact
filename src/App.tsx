5efa48dfe793ca0e6749694dff80e52dbbdcf9e5
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-01-31 11:38:18 -0500

    added info component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import Info from './components/Info'
 import ProfButton from './components/ProfButton'
 import ProfilePic from './components/ProfilePic'
 
@@ -11,6 +12,7 @@ function App() {
       <ProfilePic height='3.5rem' width='3.5rem' />
       <ProfButton text="Edit profile" />
       <ProfButton text="View archive" />
+      <Info />
     </>
   )
 }
