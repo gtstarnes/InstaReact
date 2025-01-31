92780461fcede3c8e29ea2a80b0e723534e040e5
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-01-31 11:29:40 -0500

    added additional button

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,8 @@ function App() {
   return (
     <>
       <ProfilePic height='3.5rem' width='3.5rem' />
-      <ProfButton />
+      <ProfButton text="Edit profile" />
+      <ProfButton text="View archive" />
     </>
   )
 }
