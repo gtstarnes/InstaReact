c48ab4e4fe3f211359523d2f0cb7caf8cee7baee
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-01-31 11:38:09 -0500

    supplied Info component

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -1,7 +1,30 @@
+import { user } from "../data/users"
 
+const posts = user.postList.length;
+const followers = user.followers;
+const following = user.following;
 const Info = () => {
   return (
-    <div className="container">Info</div>
+    <div className="container">
+        <div>
+            <span className="posts">
+                {posts}
+                posts
+            </span>
+        </div>
+        <button className="followers">
+            <span>
+                {followers}
+                followers
+            </span>
+        </button>
+        <button className="following">
+            <span>
+                {following}
+                following
+            </span>
+        </button>
+    </div>
   )
 }
 
